02c9da9829291dbc070fb3e761b30739e880fefc
Tabriz <77366015+Tabrizwd@users.noreply.github.com>
2021-02-09 20:05:44 +0400

    c32

---
+++ landing page/first JS/script.js
@@ -0,0 +1,11 @@
+let phrases = [
+    'отправить другу смешную гифку',
+    'посмотреть скидки на авиабилеты',
+    'разобраться, о чём поют рэперы',
+    'Юрий Дудь',
+    'расставить книги на полке по цвету',
+    'читать про зарплаты в Сан-Франциско'
+  ];
+let randIndex=Math.floor (Math.random()*phrases.length);
+console.log(randIndex);
+console.log(phrases[randIndex]);
