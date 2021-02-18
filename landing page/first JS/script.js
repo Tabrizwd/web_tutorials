e92f0eee291ec9a02079d01bfeca802430d2f088
Tabriz <77366015+Tabrizwd@users.noreply.github.com>
2021-02-19 03:55:13 +0400

    19.02

--- landing page/first JS/script.js
+++ landing page/first JS/script.js
@@ -1,16 +1,24 @@
 let phrases = [
-    'отправить другу смешную гифку',
-    'посмотреть скидки на авиабилеты',
-    'разобраться, о чём поют рэперы',
-    'Юрий Дудь',
-    'расставить книги на полке по цвету',
-    'читать про зарплаты в Сан-Франциско'
-  ];
+  'отправить другу смешную гифку',
+  'посмотреть скидки на авиабилеты',
+  'разобраться, о чём поют рэперы',
+  'Юрий Дудь',
+  'расставить книги на полке по цвету',
+  'читать про зарплаты в Сан-Франциско'
+];
 
-  function getRandomElement () {
-    
-    let randIndex = Math.floor(Math.random() * phrases.length);
-console.log(randIndex);
-document.getElementById("demo").innerHTML=phrases[randIndex];
+function getRandomElement(arr) {
+  return randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex]; 
 }
-getRandomElement();
+let randomElement = getRandomElement(phrases);
+console.log(randomElement);
+let buttonElement = document.querySelector('.button')
+let button
+let adviceElement = document.querySelector('.advice');
+let advice
+let phraseElement = document.querySelector('.phrase');
+let phrase
+let imageElement = document.querySelector('.image');
+let image
+phrase.textContent = getRandomElement(phrases);
